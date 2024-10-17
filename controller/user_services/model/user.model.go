a5e7d4eadb27e1efb2bc1bef56dfcb7233c850b0
unknown <blessforwork@gmail.com>
2024-10-17 22:56:14 +0700

    test ci/cd

---
+++ controller/user_services/model/user.model.go
@@ -0,0 +1,12 @@
+package model
+
+type User struct {
+	Id                string `db:"id" json:"fb_uid"`
+	Email             string `db:"email" json:"email"`
+	Password          string `db:"password" json:"password"`
+	DisplayName       string `db:"display_name" json:"display_name"`
+	ProfilePictureUrl string `db:"profile_picture_url" json:"profile_picture_url"`
+	UserType          string `db:"user_type" json:"user_type"`
+	CreatedAt         string `db:"created_at" json:"created_at"`
+	DeletedAt         string `db:"deleted_at" json:"deleted_at,omitempty"`
+}
