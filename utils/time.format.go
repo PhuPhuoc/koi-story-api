e99459fac207bfd3874f5d225a49e95ba462d676
unknown <blessforwork@gmail.com>
2024-10-19 18:18:39 +0700

    fix err time format in vps (linux server) env

--- utils/time.format.go
+++ utils/time.format.go
@@ -3,10 +3,10 @@ package utils
 import "time"
 
 func CreateDateTimeCurrentFormated() string {
-	currentTime := time.Now()
-	loc, _ := time.LoadLocation("Asia/Ho_Chi_Minh")
-	currentTimeInLocal := currentTime.In(loc)
-	formattedTime := currentTimeInLocal.Format("2006-01-02 15:04:05")
+	// Use time.FixedZone to create a fixed offset for Vietnam time (UTC+7)
+	vietnamZone := time.FixedZone("Asia/Ho_Chi_Minh", 7*60*60)
+	currentTime := time.Now().In(vietnamZone)
+	formattedTime := currentTime.Format("2006-01-02 15:04:05")
 
 	return formattedTime
 }
