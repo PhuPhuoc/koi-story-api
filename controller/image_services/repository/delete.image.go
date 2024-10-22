3a3974c9b02056587c9f4c4f0047da3fbed81fc2
unknown <blessforwork@gmail.com>
2024-10-22 13:57:32 +0700

    add image_id into response get all/get my consult v4

--- controller/image_services/repository/delete.image.go
+++ controller/image_services/repository/delete.image.go
@@ -3,13 +3,26 @@ package imagerepository
 import "github.com/PhuPhuoc/koi-story-api/utils"
 
 func (store *imageStore) DeleteImagePost(image_id string) error {
-	query_post := `
-    update post_image set deleted_at=?
-    where id=?
-    `
-	_, err := store.db.Exec(query_post, utils.CreateDateTimeCurrentFormated(), image_id)
+	tx, err := store.db.Beginx()
 	if err != nil {
 		return err
 	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	query_post := `
+    update post_image set post_id=?, deleted_at=?
+    where id=?
+    `
+	_, err = tx.Exec(query_post, "", utils.CreateDateTimeCurrentFormated(), image_id)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
